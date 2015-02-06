598edf8f862cd3c6ec77491030695a7eeeff5b27
Blake Provenza <baprovenza@yahoo.com>
2015-02-05 21:40:37 -0800

    Extract todo list helper

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -2,13 +2,6 @@ require 'spec_helper'
 
 describe "Adding todo items" do
 	let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries")}
-	
-	def visit_todo_list(list)
-		visit "/todo_lists"
-		within "#todo_list_#{list.id}" do
-			click_link "List Items"
-		end
-	end
 
 
 	it "is successful with valid content" do
@@ -34,7 +27,7 @@ describe "Adding todo items" do
 	it "displays an error with no content less than 2 characters long"  do
 		visit_todo_list(todo_list)
 		click_link "New Todo Item"
-		fill_in "Content", with: ""
+		fill_in "Content", with: "a"
 		click_button "Save"
 		within("div.flash") do
 			expect(page).to have_content("There was a problem adding the todo list item.")
